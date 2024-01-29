1bf228d04f5bbe4269aed70bd0ac6dc58fc11870
dulefb <101865768+dulefb@users.noreply.github.com>
2024-01-29 03:22:13 +0100

    Update index.js

--- Server-side/src/index.js
+++ Server-side/src/index.js
@@ -26,7 +26,7 @@ const server = http.createServer(async(req,res)=>{
     let path = url.parse(req.url,true);
     let queryData = path.query;
     let rootPath = path.pathname.split("/").filter((val,ind)=>ind>0);
-    let neo4jSession = neo4jDriver.session();
+    let neo4jSession = neo4jDriver.session();//neo4j sesija koja se otvara kad pristigne zahtev
     let headers = {
         'Access-Control-Allow-Origin': '*',
         'Accept':'*',
@@ -46,19 +46,129 @@ const server = http.createServer(async(req,res)=>{
         //     res.end();
         // }
         //start here
+        if(rootPath[0]=='user'){
+            res.writeHead(200,'OK',headers);
+            res.end();
+        }
         
     }
     if(req.method.toLowerCase()==='get')
     {
         //get method
-        let smh = await neo4jSession.run("match (n) return n");
-        res.writeHead(200,"OK",headers);
-        res.write(JSON.stringify(smh.records));
-        res.end();
+        if(rootPath[0]=='user'){
+            if(queryData.email && queryData.password){
+                if(queryData.label==='KORISNIK' || queryData.label==='KORISNIK'){
+                    let dbResponse = await neo4jSession.run(`match(ent:${queryData.label}{\
+                        email:$email,\
+                        password:$password\
+                    })\
+                    return ent`,{
+                        email:queryData.email,
+                        password:queryData.password
+                    });
+                    if(dbResponse){
+                        res.writeHead(200,'OK',headers);
+                        res.write(JSON.stringify(dbResponse.records[0]._fields[0].properties));
+                        res.end();
+                    }
+                    else{
+                        res.writeHead(404,'Error',headers);
+                        res.write("User not found...");
+                        res.end();
+                    }
+                }
+                else if(queryData.label!=='KORISNIK' || queryData!=='KOMPANIJA'){
+                    res.writeHead(404,'Error',headers);
+                    res.write("Invalid request...");
+                    res.end();
+                }
+            }
+        }
+        else{
+            res.writeHead(404,'Error',headers);
+            res.write('Invalid request...');
+            res.end();
+        }
     }
     if(req.method.toLowerCase()==='post')
     {
         //post method
+        if(rootPath[0]=='user'){
+            if(queryData.label==='KORISNIK'){
+                processRequestBody(req,async (dataObj)=>{
+
+                    let emailTaken = await neo4jSession.run(`match (entity:KORISNIK {
+                        email:$email\
+                    })\
+                    return entity`,{
+                        email:dataObj.email
+                    });
+                    
+                    if(emailTaken.records[0]!==undefined){
+                        res.writeHead(404,'Error',headers);
+                        res.write('Email is already taken,use another one...');
+                        res.end();
+                    }
+                    else{
+                        let dbResponse = await neo4jSession.run(`create (entity:KORISNIK {\
+                            name:$name,\
+                            lastname:$lastname,\
+                            skills:$skills,\
+                            email:$email,\
+                            password:$password\
+                        })`,{
+                            name:dataObj.name,
+                            lastname:dataObj.lastname,
+                            skills:dataObj.skills,
+                            email:dataObj.email,
+                            password:dataObj.password
+                        });
+                        res.writeHead(200,'OK',headers);
+                        res.write("Korisnik uspesno kreiran...");
+                        res.end();
+                    }
+                });
+            }
+            else if(queryData.label==='KOMPANIJA'){
+                processRequestBody(req,async (dataObj)=>{
+
+                    let emailTaken = await neo4jSession.run(`match (entity:KOMPANIJA {
+                        email:$email\
+                    })\
+                    return entity`,{
+                        email:dataObj.email
+                    });
+                    if(emailTaken.records[0]!==undefined){
+                        res.writeHead(404,'Error',headers);
+                        res.write('Email is already taken,use another one...');
+                        res.end();
+                    }
+                    else{
+                        let dbResponse = await neo4jSession.run(`create (entity:KOMPANIJA {\
+                            name:$name,\
+                            description:$description,\
+                            city:$city,\
+                            email:$email,\
+                            password:$password\
+                        })`,{
+                            name:dataObj.name,
+                            description:dataObj.description,
+                            city:dataObj.city,
+                            email:dataObj.email,
+                            password:dataObj.password
+                        });
+                        res.writeHead(200,'OK',headers);
+                        res.write("Kompanija uspesno kreirana...");
+                        res.end();
+                    }
+                });
+            }
+        }
+        else{
+            res.writeHead(404,'Error',headers);
+            res.write('Invalid request...');
+            res.end();
+        }
     }
     if(req.method.toLowerCase()==='delete')
     {
